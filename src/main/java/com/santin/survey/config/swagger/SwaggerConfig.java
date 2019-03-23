b091f5a3afa90ebbf60128afdb49d8ddfa65c41e
cesantin <cesantin@gmail.com>
2019-03-23 10:46:58 -0300

    Refactored to redirect to swagger page when base context is called

--- src/main/java/com/santin/survey/config/swagger/SwaggerConfig.java
+++ src/main/java/com/santin/survey/config/swagger/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.santin.survey.config;
+package com.santin.survey.config.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -27,11 +28,10 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     private ApiInfo metaData() {
         return new ApiInfoBuilder()
-                .title("Spring Boot REST API")
-                .description("\"Spring Boot REST API for greeting people\"")
+                .title("Survey REST API")
+                .description("\"Survey application with support to run in the cloud.\"")
                 .version("1.0.0")
-                .license("Apache License Version 2.0")
-                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .contact(new Contact("Carlos Eduardo Santin", "https://github.com/carlossantin", "cesantin@gmail.com"))
                 .build();
     }
 
